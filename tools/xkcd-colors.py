98c9ec2df7f9b4ca69ca06b843252f73f8ce632b
Jochen Voss <voss@seehuhn.de>
2014-07-23 22:42:54 +0100

    start implementing color support

---
+++ tools/xkcd-colors.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+import urllib.request
+
+print("colors = {")
+with urllib.request.urlopen('http://xkcd.com/color/rgb.txt') as f:
+    for l in f.read().decode('ASCII').splitlines():
+        name, hex = l.split('#')
+        r = int(hex[0:2], 16)
+        g = int(hex[2:4], 16)
+        b = int(hex[4:6], 16)
+        print("    '%s': (%d, %d, %d)," % (name.strip(), r, g, b))
+print("}")
