c402611aa8a55cc1cfe7b6b0599b4ea1978c8d7e
Jochen Voss <voss@seehuhn.de>
2015-10-30 09:02:51 +0000

    revert a previous, debugging-related change

--- demo1.py
+++ demo1.py
@@ -5,7 +5,7 @@ import numpy as np
 from jvplot import Plot
 
 with Plot('demo1.pdf', '4.5in', '4.5in') as fig:
-    fig.scatter_plot(np.random.rand(100),
-                     np.random.rand(100),
+    fig.scatter_plot(np.random.rand(1000),
+                     np.random.rand(1000),
                      aspect=1,
                      margin="1cm")
